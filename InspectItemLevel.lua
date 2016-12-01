dd82a89a21711bce4f25d71885d653fecdc6f291
amki <amki@amki.eu>
2016-12-01 10:01:06 +0100

    Refix bug with artifact weapon item level (#1)
    * Got itemlevel cache to work. Always prefer a fresh inspect.
    
    * Code cleanup, removed unused code.
    
    * Rebump version number
    
    * Removed lines at end of file
    
    * Fixed itemlevel for non artifact weapon combos. Non artifacts follow old rules
    
    * Removed spammy artifact weapon level output

--- InspectItemLevel.lua
+++ InspectItemLevel.lua
@@ -1,18 +1,14 @@
---version 1.2.0
+--version 1.3.0
+local Slots = {
 	"Head","Neck","Shoulder","Back","Chest","Wrist",
 	"Hands","Waist","Legs","Feet","Finger0","Finger1",
-	"Trinket0","Trinket1", "MainHand", "SecondaryHand"
+	"Trinket0","Trinket1","MainHand","SecondaryHand"
 }
 
 local InspectCache = {}
 
 local ILvlFrame = CreateFrame("Frame", "IlvlFrame")
-local lastInspectReady
-local inspecting = false
-local InspectGUID
-
 ILvlFrame:RegisterEvent("INSPECT_READY")
-ILvlFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 
 ILvlFrame:ClearAllPoints()
 ILvlFrame:SetHeight(300)
@@ -28,56 +24,83 @@ ILvlFrame:SetScript("OnEvent", function(self, event_name, ...)
 	end
 end)
 
-function ILvlFrame:INSPECT_READY(event, GUID)	
-	ILvlFrame.text:SetText(format("ilvl: ??"))
-	lastInspectReady = GetTime()
-	InspectGUID = GUID
-	inspecting = true
-	
-end
-
-function ILvlFrame:intializeItemLevelInspection()
-
+function ILvlFrame:INSPECT_READY(event, GUID)
 	if InspectFrame and InspectFrame.unit then 
 		local UnitIlevel = 0
-		if not InspectCache[InspectGUID] or InspectCache[InspectGUID].time > 800 then
-			UnitIlevel = self:GetItemLvL(InspectFrame.unit)
-			InspectCache[InspectGUID] = {time = GetTime()}
-			InspectCache[InspectGUID].ilevel = UnitIlevel
+		local cached = false
+		
+		UnitIlevel = self:GetItemLvL(InspectFrame.unit)
+		if(UnitIlevel ~= nil) then
+			InspectCache[GUID] = {time = GetTime(), ilevel = UnitIlevel}
 		else
-			UnitIlevel = InspectCache[InspectGUID].ilevel
+			if (InspectCache[GUID] ~= nil) then
+				if(InspectCache[GUID].ilevel) then
+					cached = true
+					UnitIlevel = InspectCache[GUID].ilevel
+				end
+			end
 		end
 		if InspectFrame and InspectFrame.unit then
 			ILvlFrame:SetParent(InspectFrame)
 			ILvlFrame:SetPoint("BOTTOM", InspectFrame, "RIGHT", -45, 15)
-			
-			ILvlFrame.text:SetText(format("ilvl: ".. tostring(UnitIlevel)))
+			if(cached) then
+				ILvlFrame.text:SetText(format("Cached ilvl: ".. tostring(UnitIlevel)))
+			else
+				ILvlFrame.text:SetText(format("ilvl: ".. tostring(UnitIlevel)))
+			end
 			
 		end
 	end
 end
 
-function ILvlFrame:PLAYER_TARGET_CHANGED()
-	isCalculatingIlevel = false;
+function IlvlFrame:GetArtifactWeaponLevel(unit)
+	local mainHandilvl, secondHandilvl = 0, 0
+	local itemLink = GetInventoryItemLink(unit, GetInventorySlotInfo("MainHandSlot"))
+	if (itemLink ~= nil) then
+		mainHandilvl = self:ScanForItemLevel(itemLink)
+	end
+	local itemLink = GetInventoryItemLink(unit, GetInventorySlotInfo("SecondaryHandSlot"))
+	if (itemLink ~= nil) then
+		secondHandilvl = self:ScanForItemLevel(itemLink)
+	end
+	if(mainHandilvl == secondHandilvl and mainHandilvl == 750) then
+		print("Both weapons are ilvl 750. Bug?")
+	end
+	if(mainHandilvl > secondHandilvl) then
+		return mainHandilvl
+	else
+		return secondHandilvl
+	end
 end
 
 function ILvlFrame:GetItemLvL(unit)
-	local total, item = 0, 0;
-	for i = 1, #Slots do
-		local itemLink = GetInventoryItemLink(unit, GetInventorySlotInfo(("%sSlot"):format(Slots[i])));
+	local total = 0
+	local iterate = 16
+	local mainHandLink = GetInventoryItemLink(unit, GetInventorySlotInfo("MainHandSlot"))
+	if(mainHandLink ~= nil) then
+		_,_,rarity = GetItemInfo(mainHandLink)
+		if(rarity == 6) then
+			iterate = 14
+		end
+	end
+	for i = 1, iterate do
+		local itemLink = GetInventoryItemLink(unit, GetInventorySlotInfo(("%sSlot"):format(Slots[i])))
 		if (itemLink ~= nil) then
-			local itemLevel = self:ScanForItemLevel(itemLink);
-	
+			local itemLevel = self:ScanForItemLevel(itemLink)
 			if(itemLevel and itemLevel > 0) then
-				item = item + 1;
-				total = total + itemLevel;
+				total = total + itemLevel
 			end
 		end
 	end
+	if(iterate == 14) then
+		local artilvl = self:GetArtifactWeaponLevel(unit)
+		total = total + (artilvl * 2)
+	end
+	
 	if(total < 1) then
 		return
 	end
-	return floor(total / item)
+	return floor(total / 16)
 end
 
 function IlvlFrame:GetAvailableTooltip()
@@ -88,35 +111,22 @@ function IlvlFrame:GetAvailableTooltip()
 	end
 end
 
---randomly some items dont give the correct ItemLevel a second inspectiong seems to give the correct value
 function ILvlFrame:ScanForItemLevel(itemLink)
-	local tt = self:GetAvailableTooltip();
-	tt:SetOwner(UIParent, "ANCHOR_NONE");
-	tt:SetHyperlink(itemLink);
-	tt:Show();
-	
-	local itemLevel = 0;
+	local tt = self:GetAvailableTooltip()
+	tt:SetOwner(UIParent, "ANCHOR_NONE")
+	tt:SetHyperlink(itemLink)
+	tt:Show()
+
+	local itemLevel = 0
 	for i = 2, tt:NumLines() do
-		local text = _G[ tt:GetName() .."TextLeft"..i]:GetText();
+		local text = _G[ tt:GetName() .."TextLeft"..i]:GetText()
 		if(text and text ~= "") then
-			local value = tonumber(text:match(ITEM_LEVEL:gsub( "%%d", "(%%d+)" )));
+			local value = tonumber(text:match(ITEM_LEVEL:gsub( "%%d", "(%%d+)" )))
 			if(value) then
-				itemLevel = value;
+				itemLevel = value
 			end
 		end
 	end
-	tt:Hide();
+	tt:Hide()
 	return itemLevel
 end
-
---needs a delay to make sure all INSPECT_READY has finished calling
-local function onUpdate(self,elapsed)
-	if (inspecting) then
-		if(GetTime() - lastInspectReady > .5) then
-		inspecting = false
-		ILvlFrame:intializeItemLevelInspection()
-		end
-	end
-end
-
-ILvlFrame:SetScript("OnUpdate", onUpdate)
